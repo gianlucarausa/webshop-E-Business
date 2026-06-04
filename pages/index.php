f0c5994a9854b3248416bcf520009a86242c6470
Gian-Luca Rausa <gianluca.rausa@icloud.com>
2026-06-04 11:50:22 +0200

    header export

--- pages/index.php
+++ pages/index.php
@@ -13,56 +13,12 @@
     <!--Bootstrap Icons-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <!--Favicon-->
-    <link rel="icon" type="image/png" href="./images/fav2.png">
+    <link rel="icon" type="image/png" href="../webshop/images/fav2.png">
 </head>
 
 <body class="d-flex flex-column">
     <!--Header-->
-    <header>
-        <div id="head" class="col-12 py-4 d-flex bg-danger text-white align-items-center">
-            <div class="col-8 ps-4 d-inline-flex align-items-center">
-                <image src="./images/logo.png" height="100px">
-                    <div class="p-4">
-                        <h2>Tritschler Webservices</h2>
-                        <i>est. 2026</i>
-                    </div>
-            </div>
-            <!--Datenbank API (Login / Warenkorb)-->
-            <div id="buttons" class="text-white col-4 pe-4 d-flex justify-content-end">
-                <button class="text-white btn">Login <i class="fs-5 bi bi-box-arrow-in-right"></i></button>
-                <button class="text-white btn" data-bs-toggle="offcanvas" data-bs-target="#checkout-canvas"
-                    href="#">Warenkorb <i class="fs-5 bi bi-cart2"></i></button>
-            </div>
-        </div>
-        <nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
-            <div class="container-fluid justify-content-center">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <div class="dropdown">
-                            <button type="button" class="btn text-white dropdown-toggle" data-bs-toggle="dropdown">
-                                Unsere Karte
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="category.php">Gesamte Karte</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Zu Essen</a></li>
-                                <li><a class="dropdown-item" href="#">- Pasta</a></li>
-                                <li><a class="dropdown-item" href="#">- Pizza</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Zu Trinken</a></li>
-                                <li><a class="dropdown-item" href="#">- Mate</a></li>
-                                <li><a class="dropdown-item" href="#">- Kaffee</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+    
     <!--Main-->
     <main class="col-12 bg-light">
         <?php 

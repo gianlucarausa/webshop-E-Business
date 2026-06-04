efa9fe2fc9119064b1ceb4498578b80877596887
felixreinbold <felix.reinbold2003@gmail.com>
2026-06-04 11:49:52 +0200

    includes/session_check.php

---
+++ includes/session_check.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+
+if(isset($_SESSION['last_activity'])){
+        if(time()-$_SESSION['last_activity']>600){
+            session_destroy();
+        }
+}
+
+$_SESSION['last_activity'] = time();
+
+
+function isLoggedIn() : boolean{
+
+    if(isset($_SESSION['user_id'])){
+        return true;
+    }
+    return false;
+}
+
+?>
